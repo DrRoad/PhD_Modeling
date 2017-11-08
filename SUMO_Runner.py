84e7d2d1b9d416a5c0721235adc9c1b752fe736c
wsbiko <bc3262@drexel.edu>
2017-11-08 11:50:43 -0500

    Good things happened

---
+++ SUMO_Runner.py
@@ -0,0 +1,15 @@
+import os, sys
+import numpy as np
+import traci
+import traci.constants as tc
+import re
+import SUMO_PYTHON.General_TraciSUMO as GTS
+
+
+
+#run /Users/Biko/Dropbox/PhD/Research/Models/SUMO/SUMO_DropBox/Belmont_Ave-Run/SUMO_Python-Belmont/SUMO_Runner.py
+
+#GTS.general.releaseTraci(None)
+print("general.edge_based_truck_count = ",GTS.general.edge_based_truck_count)
+#import SUMO_PYTHON.Pavement_Condition
+
